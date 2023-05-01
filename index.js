406730126b41867fd962995b72ab384c569cb7b3
Humayun63 <humayun126224@gmail.com>
2023-05-01 12:21:19 +0600

    api created

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
+const travelSpots = require('./data/travelSpots.json')
+
 app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send('Travel Guru is Running')
+})
+
+app.get('/travelspots', (req, res)=>{
+    res.send(travelSpots)
+})
+
+app.get('/travelspots/:id', (req, res) =>{
+    const id = parseFloat(req.params.id);
+    const selectedSpot = travelSpots.find(spot => parseFloat(spot.id) === id);
+    res.send(selectedSpot)
+})
+
+app.get('/hotel/:id', (req, res) =>{
+    const id = parseFloat(req.params.id)
+    const hotels = travelSpots.find(hotel => parseFloat(hotel.id) === id).hotels;
+    res.send(hotels)
 })
 
 app.listen(port, ()=>{
